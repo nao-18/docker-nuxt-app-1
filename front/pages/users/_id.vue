e54bab19da1b849370aa098ed0517064b31340d9
nao-18 <kyahyo17@yahoo.co.jp>
2021-02-03 15:39:20 +0900

    routing

---
+++ front/pages/users/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container">
+    <h1>{{ message}}</h1>
+    <p>id: {{ this.$route.params.id }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "_id",
+  data: function () {
+    return {
+      message: "/users/_id.vue",
+    }
+  },
+  validate({params}) {
+    return /^\d+$/.test(params.id)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
